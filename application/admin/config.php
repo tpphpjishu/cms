cc485a08d5ba7d3714a69dc65461beb6a0250263
tpphpjishu <912439005@qq.com>
2017-12-28 02:47:28 +0800

    application

---
+++ application/admin/config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+        'template' => [
+            // 模板后缀
+            'view_suffix'  => 'htm',
+        ],
+    "view_replace_str" =>[
+        '__ADMIN__'=>'/cms/public/static/admin'
+    ],
+];
