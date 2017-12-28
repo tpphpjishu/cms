525f9e71df2fc013b9226650756162fbc81857a3
root <912439005@qq.com>
2017-12-28 12:30:09 +0800

    admin

---
+++ application/admin/controller/Admin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/28
+ * Time: 12:08
+ */
+
+namespace app\admin\controller;
+
+
+class Admin
+{
+    public function lst(){
+
+    }
+
+}
